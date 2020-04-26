4c32c5e6ff5bfabd9d0580dbf9edd678b309247a
Тихонин Александр <tihonina@oooinex.ru>
2020-04-26 21:48:40 +0300

    HW4: Вызов Resume.toString через рефлексию

---
+++ src/ru/javawebinar/basejava/webapp/MainReflection.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava.webapp;
+
+import ru.javawebinar.basejava.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+
+        // invoke r.toString via reflection
+        System.out.println(r.getClass().getDeclaredMethod("toString").invoke(r));
+    }
+}
