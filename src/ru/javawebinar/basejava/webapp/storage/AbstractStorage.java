c748fae4354bafc81e3d7f9e97fdc6037ce43968
Тихонин Александр <tihonina@oooinex.ru>
2020-05-09 21:09:48 +0300

    AbstractStorage: логи для основных операций

--- src/ru/javawebinar/basejava/webapp/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/webapp/storage/AbstractStorage.java
@@ -6,26 +6,32 @@ import ru.javawebinar.basejava.webapp.model.Resume;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     public Resume get(String uuid) {
+        LOG.info("Get storage: " + uuid);
         exceptionIfNotExist(uuid);
         return getFromStorage(getSearchKey(uuid));
     }
 
     public void update(Resume resume) {
+        LOG.info("Update storage: " + resume.toString());
         exceptionIfNotExist(resume.getUuid());
         SK searchKey = getSearchKey(resume.getUuid());
         updateInStorage(searchKey, resume);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete storage: " + uuid);
         exceptionIfNotExist(uuid);
         deleteFromStorage(getSearchKey(uuid));
     }
 
     public void save(Resume resume) {
+        LOG.info("Save storage: " + resume.toString());
         exceptionIfExist(resume.getUuid());
         saveToStorage(resume);
     }
