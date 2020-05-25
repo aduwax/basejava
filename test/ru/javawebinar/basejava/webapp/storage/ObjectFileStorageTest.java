2afafa9f09786e9c1733d0ed950ad9c459ee8db1
Тихонин Александр <tihonina@oooinex.ru>
2020-05-25 09:15:23 +0300

    Fix ObjectStreamSerializer tests

--- test/ru/javawebinar/basejava/webapp/storage/ObjectFileStorageTest.java
+++ test/ru/javawebinar/basejava/webapp/storage/ObjectFileStorageTest.java
@@ -1,9 +1,9 @@
 package ru.javawebinar.basejava.webapp.storage;
 
-import ru.javawebinar.basejava.webapp.storage.strategy.StorageObjectStreamReadWriteStrategy;
+import ru.javawebinar.basejava.webapp.storage.serializer.ObjectStreamSerializer;
 
 public class ObjectFileStorageTest extends AbstractStorageTest {
     public ObjectFileStorageTest() {
-        super(new FileStorage(STORAGE_DIR, new StorageObjectStreamReadWriteStrategy()));
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamSerializer()));
     }
 }
